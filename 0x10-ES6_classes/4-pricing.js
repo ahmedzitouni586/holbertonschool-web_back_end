2c59170ba809684ace8fcdea247c76d090f76668
ahmedzitouni586 <zitouniahmed595@gmail.com>
2022-08-04 03:37:07 +0100

    done

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Princing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
