1263ede7a0ec6df202a54cc2a88be55cad33da35
Ahmed <zitouniahmed595@gmail.com>
2022-08-18 08:36:13 +0100

    done

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,9 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
     it('it should return the sum', function() {
-        assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1, 3.7), 5);
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-        assert.equal(calculateNumber(1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     })
 });
