090a0bf89789ec381688c647590b53b65fa41c02
Ahmed <zitouniahmed595@gmail.com>
2022-09-09 16:34:10 +0100

    complex http server

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,24 +1,21 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const dbfile = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  const { url } = req;
-  const message = async () => {
-    await countStudents(process.argv[2]);
-  };
-  if (url === '/') res.end('Hello Holberton School!');
-  else if (url === '/students') {
-    try {
-      const response = `This is the list of our students\n${message}`;
-      res.end(response);
-    } catch (err) {
-      res.end(`${err.message}\n`);
-    }
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    await dbfile(process.argv[2])
+      .then((success) => {
+        const output = `This is the list of our students\n${success}`;
+        res.end(output);
+      })
+      .catch((err) => {
+        res.write('This is the list of our students\n');
+        res.end(err.message);
+      });
   }
-});
-
-app.listen(1245);
+}).listen(1245);
 
 module.exports = app;
