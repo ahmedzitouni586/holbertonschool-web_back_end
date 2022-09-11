8947ed8c929ae84ea4c5e634ad155908dd900412
Ahmed <zitouniahmed595@gmail.com>
2022-09-11 22:08:21 +0100

    basic mocha test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,12 @@
 const assert = require('assert');
+
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('it should return the sum', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  })
+describe('calculateNumber', () => {
+    it('should return the sum', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
 });
