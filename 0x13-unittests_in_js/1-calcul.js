85092dbe4c5ddd20e2868c0af694dbb4ccefeda4
Ahmed <zitouniahmed595@gmail.com>
2022-09-11 22:40:10 +0100

    combining tests

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,14 +1,14 @@
 module.exports = function calculateNumber(type, a, b) {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
-  else if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  }
-  else if (type === 'DIVIDE') {
-    if (b === 0) {
-      return 'Error'
+    if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
     }
-    return Math.round(a) / Math.round(b);
-  } 
+    else if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+    }
+    else if (type === 'DIVIDE') {
+        if (Math.round(b) === 0) {
+            return 'Error';
+        }
+        return Math.round(a) / Math.round(b);
+    }
 }
--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,17 +1,18 @@
 const assert = require('assert');
+
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function() {
-  it('should return the sum', function() {
-    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
-  })
-  it('should subtract the numbers', function() {
-    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-  })
-  it('should divide the numbers', function() {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-  })
-  it('should return Error', function() {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error')
-  })
+describe('calculaNumber', () => {
+    it('should return the sum', () => {
+        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+    it('should return the subtraction', () => {
+        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+    it('should return the division', () => {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+    it('should return Error string', () => {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
 });
