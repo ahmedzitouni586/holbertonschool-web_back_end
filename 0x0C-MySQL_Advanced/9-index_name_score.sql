5d4b10933890102fbac3521ba7e0cea1bcbd0925
Ahmed <zitouniahmed595@gmail.com>
2023-02-16 14:04:49 +0100

    done

--- 0x0C-MySQL_Advanced/9-index_name_score.sql
+++ 0x0C-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- script that creates an index idx_name_first
 -- index the first letter of name and the score
 
-CREATE INDEX idx_name_first ON names (name(1), score);
+CREATE INDEX idx_name_first_score ON names ( name(1), score );
